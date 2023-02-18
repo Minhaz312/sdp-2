687db6e68de74980ca9f383e2c9b1b7affa1c2ef
Mohammad Minhaz <minhazkhan312@gmail.com>
2023-02-18 13:17:17 +0600

    second commit

---
+++ src/components/Layout.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { FaUser } from "react-icons/fa"
+import { FiYoutube } from "react-icons/fi"
+import { AiFillHome } from "react-icons/ai"
+import { BsCartFill, BsInstagram } from "react-icons/bs"
+import { RiFacebookFill } from "react-icons/ri"
+export default function Layout({children}) {
+
+    const [showSuggestion, setShowSuggestion] = useState(false)
+    const handleSearchSuggestion = () => {
+        setShowSuggestion(true)
+    }
+    const handleCloseSearchSuggestion = () => {
+        setShowSuggestion(false)
+    }
+
+  return (
+    <div className='bg-slate-100 min-h-screen m-0 p-0 w-full'>
+        <div className='w-full bg-white shadow-sm py-0 px-5 sticky top-0 z-20'>
+            <nav className='w-4/5 mx-auto flex items-center py-2'>
+                <div className='flex w-full'>
+                    <div className='mr-5'>
+                        <h1 className='font-bold text-4xl'>BGCExpress</h1>
+                    </div>
+                    <div className='w-full px-32'>
+                        <div className='relative w-full'>
+                            <div className='flex items-center w-full'>
+                                <input type="search" placeholder='Search your products...' className='search-input border-none bg-slate-100 w-full transition-all px-3 py-4 outline-none' onBlur={handleCloseSearchSuggestion} onFocus={handleSearchSuggestion} />
+                                <button className='px-3 py-4 outline-none border-none bg-black text-white font-semibold'>Search</button>
+                            </div>
+                            <div className={`${showSuggestion?"block":"hidden"} h-32 bg-white shadow-lg w-full absolute top-full z-20 left-0 right-0`}>
+                                <span className='block text-center text-slate-500 mt-14'>start typing...</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='p-5'>
+                    <ul className='flex gap-5 items-center'>
+                        <li>
+                            <NavLink className='flex items-center gap-1 text-xl'><AiFillHome />Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className='flex items-center gap-1 text-xl'><BsCartFill />Cart</NavLink>
+                        </li>
+                        <li>
+                            <NavLink className='flex items-center font-bold gap-1 text-xl'><FaUser />Signin</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div className='w-4/5 mx-auto'>
+            {children}
+        </div>
+        <div className='bg-slate-900 py-5'>
+            <div className='w-4/5 mx-auto'>
+                <div className='grid grid-cols-12 gap-3'>
+                    <div className='col-span-3 text-white'>
+                        <h1 className='font-bold text-4xl'>BGCExpress</h1>
+                        <p className='text-sm text-slate-300 my-2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                        <div className='flex gap-2 my-2'>
+                            <BsInstagram color='white' />
+                            <RiFacebookFill color='white' />
+                            <FiYoutube color='white' />
+                        </div>
+                    </div>
+                    <div className='col-span-3 text-white'>
+                        <h3 className='text-lg font-bold'>Quick Links</h3>
+                        <ul className='text-sm pl-3 text-slate-300'>
+                            <li className='my-2 hover:text-slate-100'>Home</li>
+                            <li className='my-2 hover:text-slate-100'>Home</li>
+                            <li className='my-2 hover:text-slate-100'>Home</li>
+                            <li className='my-2 hover:text-slate-100'>Home</li>
+                            <li className='my-2 hover:text-slate-100'>Home</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='border-t border-slate-600 text-slate-300 my-3 py-3 text-center'>
+                    Copyright&copy; 2000-2023. All right reserved.
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+    const settings = {
+        dots: false,
+        infinite: true,
+        arrows:false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        pausonHover:true,
+        speed: 3000,
+        autoplaySpeed: 3000,
+      };
+      const categories = [
+        {name:"category 1"},
+        {name:"category 1"},
+        {name:"category 1"},
+        {name:"category 1"},
+        {name:"category 1"},
+        {name:"category 1"},
+        {name:"category 1"},
+        {name:"category 1"},
+      ]
+  return (
+    <Layout>
+        <div className='grid grid-cols-12 h-full bg-white'>
+            <div className='col-span-2'>
+                <ul className='px-5'>
+                    {
+                        categories.map((item,i)=><li key={i} className='py-2 text-slate-400 font-semibold hover:text-slate-800'><Link to="/asdf" className='w-full'>{item.name}</Link></li>)
+                    }
+                </ul>
+            </div>
+            <div className='col-span-8 h-full'>
+                <Slider {...settings}>
+                    <div style={{height:"400px"}}>
+                        <img className='w-full object-cover' style={{height:"400px"}} src="https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1200&format=pjpg&exif=1&iptc=1" />
+                    </div>
+                    <div style={{height:"400px"}}>
+                        <img className='w-full object-cover' style={{height:"400px"}} src="https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1200&format=pjpg&exif=1&iptc=1" />
+                    </div>
+                    <div style={{height:"400px"}}>
+                        <img className='w-full object-cover' style={{height:"400px"}} src="https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1200&format=pjpg&exif=1&iptc=1" />
+                    </div>
+                    <div style={{height:"400px"}}>
+                        <img className='w-full object-cover' style={{height:"400px"}} src="https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1200&format=pjpg&exif=1&iptc=1" />
+                    </div>
+                </Slider>
+            </div>
+            <div className='col-span-2 h-full'>
+                <div className='h-full w-full'>
+                    <div className='bg-slate-200 rounded shadow m-1' style={{height:"200px"}}>
+                        item 1
+                    </div>
+                    <div className='bg-slate-200 rounded shadow m-1' style={{height:"200px"}}>
+                        item 1
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='my-3 bg-slate-200 p-3 shadow-sm rounded'>
+            <h3 className='text-xl font-bold text-slate-500 my-3'>Suggested for you</h3>
+            <div className='grid grid-cols-12 gap-2'>
+                {
+                    [1,2,3,4,5,6,7,8,9,10,11,12].map((item,i)=><div key={i} className='col-span-2 p-3 bg-white rounded hover:shadow-lg'>
+                    <img src="https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1200&format=pjpg&exif=1&iptc=1" height="150px" />
+                    <h3 className='text-slate-500 text-base'>lorem ipsum dollar sit ammet, lorem ipsum dollar sit ammet...</h3>
+                </div>)
+                }
+            </div>
+            <div className='flex justify-center mt-5'>
+                <button className='border-none outline-none px-2 py-1 text-sm font-semibold text-white bg-slate-900 rounded focus:outline-green-900'>see more</button>
+            </div>
+        </div>
+    </Layout>
+  )
+}
